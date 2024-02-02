bb145fa28306fc5341e7dd9edd8134f50945fa5c
Tushar Gupta <tushargupta4870@gmail.com>
2024-02-02 12:22:37 +0530

    installed react vite and react router dom

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <div className='pages'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+const Home = () => {
+    return (
+        <div className="home"> <h2>Home</h2> </div>
+    )
+}
+export default Home
